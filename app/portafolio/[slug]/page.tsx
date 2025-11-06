a3fbbc885873283ffecb75bbf28af2379c632307
Pedro Molina <pedrom@pekebyte.com>
2025-11-06 04:35:57 +0100

    deleting console.log

--- app/portafolio/[slug]/page.tsx
+++ app/portafolio/[slug]/page.tsx
@@ -55,7 +55,6 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
 export default async function Page(props: { params: Promise<{ slug: string }> }) {
   const { slug } = await props.params;
   const project = await getPortfolioItem(slug);
-  console.log("project", project['portfolio-category']);
   const projectCategories = await getPortfolioItemCategories(project['portfolio-category'] || []);
   if (!project) {
     notFound();
